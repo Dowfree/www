ed6a5893189d8cbecdcfff1d7582400636f437cc
Dowfree <923299213@qq.com>
2017-04-11 20:56:39 +0800

    complie ORM, perfect app.py and .gitigore

--- app.py
+++ app.py
@@ -7,16 +7,20 @@ __author__ = 'Dowfree'
 async web application.
 '''
 
-import logging; logging.basicConfig(level=logging.INFO)
+import logging
+logging.basicConfig(level=logging.INFO)
 
-import asyncio, os, json, time
+import asyncio
+import os
+import json
+import time
 from datetime import datetime
 
 from aiohttp import web
 
 
 def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
+    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
 
 async def init(loop):
     app = web.Application(loop=loop)
