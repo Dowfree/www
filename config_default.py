31f11dec2be0414a178cf4116c5af345b4d154ef
Dowfree <923299213@qq.com>
2017-04-15 16:13:38 +0800

    compile configuration files

---
+++ config_default.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Default configurations.
+"""
+
+__author__ = 'Dowfree'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'awesome-python3-webapp',
+        'password': 'webapp_mysql',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
---
+++ config_override.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Override configurations.
+"""
+
+__author__ = 'Dowfree'
+
+configs = {
+    'db': {
+        'host': '192.168.0.100'
+    }
+}
